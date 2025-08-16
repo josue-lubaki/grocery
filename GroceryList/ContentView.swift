e72585db3894ac9c8f1c024c59cadc52b6a28ebd
Josue Lubaki <josuelubaki@gmail.com>
2025-08-16 16:02:46 -0400

    Initial Commit

---
+++ GroceryList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GroceryList
+//
+//  Created by Josue Lubaki on 2025-08-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GroceryList/GroceryListApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryListApp.swift
+//  GroceryList
+//
+//  Created by Josue Lubaki on 2025-08-16.
+//
+
+import SwiftUI
+
+@main
+struct GroceryListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
